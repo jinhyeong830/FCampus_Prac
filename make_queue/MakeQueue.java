e0867dff3ff59baecc2f2bb75c542a02bd69cf6c
jinhyeong830 <shg02387@naver.com>
2022-06-22 23:30:18 +0900

    make QUEUE by using arraylist

--- make_queue/MakeQueue.java
+++ make_queue/MakeQueue.java
@@ -1,12 +1,41 @@
 package make_queue;
 
+import java.util.ArrayList;
+
 public class MakeQueue<T> {
+	private ArrayList<T> queue=new ArrayList<T>();
 	
-	public <T> MakeQueue(){
-		
+	//enqueue, dequeue, isempty 메소드 구현
+	public void Enqueue(T item) {
+		queue.add(item);
 	}
+	
+	public T Dequeue() {
+		if(queue.isEmpty()) {
+			return null;
+		}
+		return queue.remove(0);
+	}
+	
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+	
+	//main
 
 	public static void main(String[] args) {
+		MakeQueue<Integer> q=new MakeQueue<Integer>();
+		
+		q.Enqueue(1);
+		q.Enqueue(2);
+		q.Enqueue(5);
+		
+		System.out.println(q.Dequeue());
+		System.out.println(q.Dequeue());
+		System.out.println(q.Dequeue());
+		
+		//결과: fifo->1 2 5 순으로 나올것..
+
 
 
 		
