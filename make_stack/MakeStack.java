c80d5e3f9558cccf4f79882099d8f1b5059e8e24
jinhyeong830 <shg02387@naver.com>
2022-07-07 18:58:48 +0900

    make stack by using ArrayList

--- make_stack/MakeStack.java
+++ make_stack/MakeStack.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 public class MakeStack<T> {
 //제네릭 타입 사용시 <T> 를 클래스 뒤에 써줘야 하는걸까?
 	
-//	private로 어레이 리스트 선언
-	private ArrayList<T> stack_arr=new ArrayList<T>();
+//제네릭 타입 사용시 <T> 를 클래스 뒤에 써줘야 하는걸까?
+		
+	//	private로 어레이 리스트 선언
+		private ArrayList<T> stack_arr=new ArrayList<T>();
 
 	//push pop isEmpty메소드 구현
 	
